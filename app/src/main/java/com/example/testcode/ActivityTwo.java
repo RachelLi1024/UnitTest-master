ba588bfdb26fb9fe998e2e33ce3df8d5acd4e832
RachelLi <357667228@qq.com>
2019-03-31 17:33:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/testcode/ActivityTwo.java
@@ -0,0 +1,11 @@
+package com.example.testcode;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityTwo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
